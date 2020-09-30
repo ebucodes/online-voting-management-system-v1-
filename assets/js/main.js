73fb41054dd67e1f27d9ce35781321cb9b9e8afe
Chukwuebuka Ohaji <62135508+ebukatansi@users.noreply.github.com>
2020-09-30 01:04:37 +0100

    Add files via upload

---
+++ assets/js/main.js
@@ -0,0 +1,96 @@
+			$(document).ready(function() {
+			    $(".cf").on("change", function() {
+			        if ($(".cf:checked").length == 1) {
+			            $(".cf").attr("disabled", "disabled");
+			            $(".cf:checked").removeAttr("disabled");
+			        } else {
+			            $(".cf").removeAttr("disabled");
+			        }
+			    });
+
+			    $(".lwf").on("change", function() {
+			        if ($(".lwf:checked").length == 1) {
+			            $(".lwf").attr("disabled", "disabled");
+			            $(".lwf:checked").removeAttr("disabled");
+			        } else {
+			            $(".lwf").removeAttr("disabled");
+			        }
+			    });
+
+			    $(".rwf").on("change", function() {
+			        if ($(".rwf:checked").length == 1) {
+			            $(".rwf").attr("disabled", "disabled");
+			            $(".rwf:checked").removeAttr("disabled");
+			        } else {
+			            $(".rwf").removeAttr("disabled");
+			        }
+			    });
+
+			    $(".amf").on("change", function() {
+			        if ($(".amf:checked").length == 1) {
+			            $(".amf").attr("disabled", "disabled");
+			            $(".amf:checked").removeAttr("disabled");
+			        } else {
+			            $(".amf").removeAttr("disabled");
+			        }
+			    });
+
+			    $(".cmf").on("change", function() {
+			        if ($(".cmf:checked").length == 1) {
+			            $(".cmf").attr("disabled", "disabled");
+			            $(".cmf:checked").removeAttr("disabled");
+			        } else {
+			            $(".cmf").removeAttr("disabled");
+			        }
+			    });
+
+			    $(".dmf").on("change", function() {
+			        if ($(".dmf:checked").length == 1) {
+			            $(".dmf").attr("disabled", "disabled");
+			            $(".dmf:checked").removeAttr("disabled");
+			        } else {
+			            $(".dmf").removeAttr("disabled");
+			        }
+
+			    });
+			    $(".cb_1").on("change", function() {
+			        if ($(".cb_1:checked").length == 1) {
+			            $(".cb_1").attr("disabled", "disabled");
+			            $(".cb_1:checked").removeAttr("disabled");
+			        } else {
+			            $(".cb_1").removeAttr("disabled");
+			        }
+			    });
+			    $(".cb_2").on("change", function() {
+			        if ($(".cb_2:checked").length == 1) {
+			            $(".cb_2").attr("disabled", "disabled");
+			            $(".cb_2:checked").removeAttr("disabled");
+			        } else {
+			            $(".cb_2").removeAttr("disabled");
+			        }
+			    });
+			    $(".lb").on("change", function() {
+			        if ($(".lb:checked").length == 1) {
+			            $(".lb").attr("disabled", "disabled");
+			            $(".lb:checked").removeAttr("disabled");
+			        } else {
+			            $(".lb").removeAttr("disabled");
+			        }
+			    });
+			    $(".rb").on("change", function() {
+			        if ($(".rb:checked").length == 1) {
+			            $(".rb").attr("disabled", "disabled");
+			            $(".rb:checked").removeAttr("disabled");
+			        } else {
+			            $(".rb").removeAttr("disabled");
+			        }
+			    });
+			    $(".gk").on("change", function() {
+			        if ($(".gk:checked").length == 1) {
+			            $(".gk").attr("disabled", "disabled");
+			            $(".gk:checked").removeAttr("disabled");
+			        } else {
+			            $(".gk").removeAttr("disabled");
+			        }
+			    });
+			});
